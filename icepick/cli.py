956face341b9b47283afa5f8cf419be2a6f58b6b
iceSEC <icesec@atomicmail.io>
2026-08-23 14:49:28 +0330

    cli.py

--- icepick/cli.py
+++ icepick/cli.py
@@ -6,7 +6,6 @@ by iceSEC | Cyber Intelligence Operations
 import sys
 import os
 
-# اضافه کردن مسیر ریشه‌ی پروژه به sys.path
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from gui_standalone import IcePickPro
